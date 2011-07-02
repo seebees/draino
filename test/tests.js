122494f4c904988450c58569432bd20a5b6d10aa
seebees <seebees@gmail.com>
2011-07-01 21:21:39 -0700

    massive changes that I don't want to go into
    to short answer is each operation will now be implemented by a single engine and the engine gets its parameters from a single option setter.
    
    basically this is a kind of extensive use of curry.
    
    Also the files are separated and the tests needed to be rewritten.
    
    Signed-off-by: seebees <seebees@gmail.com>

--- test/tests.js
+++ test/tests.js
@@ -3,82 +3,29 @@ var assert          = require('assert'),
     EventEmitter    = require('events').EventEmitter,
     vows            = require('vows'),
     draino          = require('draino'),
-    simple          = function (flushEvent, endEvent, chunks, delay) {
+    simple          = function (emptyEvent, endEvent, chunks, delay) {
         var self = this;
         
         EventEmitter.call(self);
         chunks.forEach(function (chunk, i) {
             setTimeout(function () {
-                self.emit(flushEvent, chunk);
+                self.emit(emptyEvent, chunk);
             }, delay*i);
         });
         
         setTimeout(function () {
             self.emit(endEvent);
         }, delay*(chunks.length));
+    },
+    ShinnyEmitter = function () {
+        EventEmitter.call(self);
     };
         
 util.inherits(simple, EventEmitter);
+util.inherits(ShinnyEmitter, EventEmitter);
 
 vows.describe('how to use draino').addBatch(
 {
-    'Calling flush' : {
-        topic : function(){
-            this.callback(null, draino.flush());
-        },
-        'will return a decorated EventEmitter.' : function(error, emitter) {
-            assert.ok(emitter instanceof EventEmitter);
-            assert.ok(emitter.hasOwnProperty('flush'));
-            assert.ok(emitter.hasOwnProperty('from'));
-            assert.ok(emitter.hasOwnProperty('to'));
-            assert.ok(emitter.hasOwnProperty('toMe'));
-            assert.ok(typeof emitter.flush === 'function');
-            assert.ok(typeof emitter.from === 'function');
-            assert.ok(typeof emitter.to === 'function');
-            assert.ok(typeof emitter.toMe === 'function');
-        },
-        '.from(readStream).toMe()' : {
-            topic : function(shinyEmitter) {
-                return shinyEmitter.from(
-                    new simple('data', 'end', ['one','two','three'], 5)
-                ).toMe();
-            },
-            'will return the data' : function (error, data) {
-                assert.strictEqual(data, 'onetwothree');
-            }
-        }
-    },
-    'Passing flush an EventEmitter' : {
-        topic : function(){
-            var myEmitter = new EventEmitter();
-            this.callback(null, [draino.flush(myEmitter), myEmitter]);
-        },
-        'will decorate the EventEmitter you pass.' : function(error, data) {
-            var shinyEmitter = data[0], 
-                originalEmitter = data[1];
-            assert.ok(shinyEmitter instanceof EventEmitter);
-            assert.ok(shinyEmitter === originalEmitter);
-            assert.ok(shinyEmitter.hasOwnProperty('flush'));
-            assert.ok(shinyEmitter.hasOwnProperty('from'));
-            assert.ok(shinyEmitter.hasOwnProperty('to'));
-            assert.ok(shinyEmitter.hasOwnProperty('toMe'));
-            assert.ok(typeof shinyEmitter.flush === 'function');
-            assert.ok(typeof shinyEmitter.from === 'function');
-            assert.ok(typeof shinyEmitter.to === 'function');
-            assert.ok(typeof shinyEmitter.toMe === 'function');
-        },
-        ', then calling .from(readStream).toMe()' : {
-            topic : function(emitters) {
-                var originalEmitter = emitters[1];
-                return originalEmitter.flush().from(
-                    new simple('data', 'end', ['one','two','three'], 5)
-                ).toMe();
-            },
-            'will return the data' : function (error, data) {
-                assert.strictEqual(data, 'onetwothree');
-            }
-        }
-    },
     'Shinning an EventEmitter' : {
         topic : function(){
             var myEmitter = new EventEmitter();
@@ -86,87 +33,210 @@ vows.describe('how to use draino').addBatch(
             draino.shine(myEmitter);
             this.callback(null, myEmitter);
         },
-        'will decorate the EventEmitter with flush.' : function(error, shinyEmitter) {
+        'will decorate the EventEmitter.' : function(error, shinyEmitter) {
             assert.ok(shinyEmitter instanceof EventEmitter);
-            assert.ok(shinyEmitter.hasOwnProperty('flush'));
+            assert.ok(shinyEmitter.hasOwnProperty('empty'));
+            
+            
             assert.ok(!shinyEmitter.hasOwnProperty('from'));
             assert.ok(!shinyEmitter.hasOwnProperty('to'));
             assert.ok(!shinyEmitter.hasOwnProperty('toMe'));
+            assert.ok(typeof shinyEmitter.empty === 'function');
+        }
+    },
+    'Shinning an class that inherits from EventEmitter' : {
+        topic : function(){
+            draino.shinePrototype(ShinnyEmitter);
+            this.callback(null, ShinnyEmitter);
+        },
+        'will return will decorate the class.' : function(error, ShinnyEmitter) {
+            assert.ok(ShinnyEmitter.prototype.hasOwnProperty('empty'));
+            assert.ok(typeof ShinnyEmitter.prototype.empty === 'function');
+            
+            assert.ok(!ShinnyEmitter.prototype.hasOwnProperty('to'));
+            assert.ok(!ShinnyEmitter.prototype.hasOwnProperty('from'));
+            assert.ok(!ShinnyEmitter.prototype.hasOwnProperty('isDry'));
+            assert.ok(!ShinnyEmitter.prototype.hasOwnProperty('onDone'));
+            assert.ok(!ShinnyEmitter.prototype.hasOwnProperty('onEach'));
+            assert.ok(!ShinnyEmitter.prototype.hasOwnProperty('onAccumulate'));
+            assert.ok(!ShinnyEmitter.prototype.hasOwnProperty('toMe'));
+            assert.ok(!ShinnyEmitter.prototype.hasOwnProperty('flush'));
+        }
+    },
+    'Calling empty()' : {
+        topic : function(){
+            this.callback(null, draino.empty());
+        },
+        'will return an option setter for empty().' : function(error, op) {
+            assert.ok(op.hasOwnProperty('to'));
+            assert.ok(op.hasOwnProperty('from'));
+            assert.ok(op.hasOwnProperty('isDry'));
+            assert.ok(op.hasOwnProperty('onDone'));
+            assert.ok(op.hasOwnProperty('onEach'));
+            assert.ok(op.hasOwnProperty('onAccumulate'));
+            assert.ok(op.hasOwnProperty('toMe'));
+            assert.ok(op.hasOwnProperty('flush'));
+            
+            assert.ok(typeof op.to === 'function');
+            assert.ok(typeof op.from === 'function');
+            assert.ok(typeof op.isDry === 'function');
+            assert.ok(typeof op.onDone === 'function');
+            assert.ok(typeof op.onEach === 'function');
+            assert.ok(typeof op.onAccumulate === 'function');
+            assert.ok(typeof op.toMe === 'function');
+            assert.ok(typeof op.flush === 'function');
+        },
+    },
+    'Passing empty() an EventEmitter' : {
+        topic : function(){
+            var myEmitter = new EventEmitter();
+            this.callback(null, [draino.empty(myEmitter), myEmitter]);
+        },
+        'will decorate the EventEmitter you pass with an option setter for empty().' : function(error, data) {
+            var shinyEmitter = data[0], 
+                originalEmitter = data[1];
+            
+            assert.ok(shinyEmitter instanceof EventEmitter);
+            assert.ok(shinyEmitter === originalEmitter);
+            assert.ok(shinyEmitter.hasOwnProperty('to'));
+            assert.ok(shinyEmitter.hasOwnProperty('from'));
+            assert.ok(shinyEmitter.hasOwnProperty('isDry'));
+            assert.ok(shinyEmitter.hasOwnProperty('onDone'));
+            assert.ok(shinyEmitter.hasOwnProperty('onEach'));
+            assert.ok(shinyEmitter.hasOwnProperty('onAccumulate'));
+            assert.ok(shinyEmitter.hasOwnProperty('toMe'));
+            assert.ok(shinyEmitter.hasOwnProperty('flush'));
+            
+            assert.ok(typeof shinyEmitter.to === 'function');
+            assert.ok(typeof shinyEmitter.from === 'function');
+            assert.ok(typeof shinyEmitter.isDry === 'function');
+            assert.ok(typeof shinyEmitter.onDone === 'function');
+            assert.ok(typeof shinyEmitter.onEach === 'function');
+            assert.ok(typeof shinyEmitter.onAccumulate === 'function');
+            assert.ok(typeof shinyEmitter.toMe === 'function');
             assert.ok(typeof shinyEmitter.flush === 'function');
-        },
-        ', then calling flush().from(readStream).toMe()' : {
-            topic : function(shinyEmitter) {
-                return shinyEmitter.flush().from(
-                    new simple('data', 'end', ['one','two','three'], 5)
-                ).toMe();
-            },
-            'will return the data' : function (error, data) {
-                assert.strictEqual(data, 'onetwothree');
-            }
-        }
-    },
-    
-    'Fush with explicit paramaters' : {
-        topic : function(){
-            var s = new simple('data', 'end', ['one','two','three'], 5),
-                c = new EventEmitter();
             
-            draino.flush('data').from(s).isDry('end').onEach('success').to(c);
-            return c;
-        },
-        'will return all the data.' : function(error, data) {
-            assert.strictEqual(data, 'onetwothree');
         }
     },
-    'Fush with default paramaters' : {
-        topic : function(){
-            var s = new simple('data', 'end', ['one','two','three'], 5),
-                c = new EventEmitter();
-            
-            draino.flush(s).to(c);
-            return c;
-        },
-        'will return all the data.' : function(error, data) {
-            assert.strictEqual(data, 'onetwothree');
-        }
-    },
-    'Flushing a "read stream" toMe' : {
+    'Emptining a "read stream" to itself' : {
         topic : function(){
             var s = new simple('data', 'end', ['one','two','three'], 5);
             
-            return draino.flush().from(s).toMe();
+            return draino.empty(s).toMe().flush();
         },
         'will return all the data.' : function(error, data) {
             assert.strictEqual(data, 'onetwothree');
         }
     },
-    'Flushing a "read stream," flushing to itself' : {
+    'Emptining a "read stream" to a target' : {
         topic : function(){
-            var s = new simple('data', 'end', ['one','two','three'], 5);
+            var s = new simple('data', 'end', ['one','two','three'], 5)
+                target = new EventEmitter();
             
-            return draino.flush(s).toMe();
+            draino.empty(s).to(target).flush();
+            
+            return target;
         },
         'will return all the data.' : function(error, data) {
             assert.strictEqual(data, 'onetwothree');
         }
     },
-    'Flushing arbitrary events' : {
+    'Using an option setter to empty a "read stream"' : {
         topic : function(){
-            var s = new simple('myEvent', 'iBeDone', ['one','two','three'], 5);
+            var s = new simple('data', 'end', ['one','two','three'], 5)
+                target = new EventEmitter(),
+                op = draino.empty();
             
-            return draino.flush(s,'myEvent').from('iBeDone').toMe('hotDAM').on('hotDAM', function (data) {
+            op.from(s).to(target).flush();
+            
+            return target;
+        },
+        'will return all the data.' : function(error, data) {
+            assert.strictEqual(data, 'onetwothree');
+        }
+    },
+    'Emptying arbitrary events' : {
+        topic : function(){
+            var s = new simple('myData', 'BeDone', ['one','two','three'], 5)
+                target = new EventEmitter();
+            
+            draino.empty('myData').
+                from(s).
+                isDry('BeDone').
+                to(target).
+                onEach('HotDAM').
+                flush();
+            
+            target.on('HotDAM', function (data) {
                 this.emit('success', data);
             });
+            
+            return target;
         },
-        'will return all the data' : function (error, data) {
+        'will return all the data.' : function(error, data) {
             assert.strictEqual(data, 'onetwothree');
+        }
+    },
+    'Flushing N "read streams"' : {
+        topic : function () {
+            var accumulate = [],
+                target = new EventEmitter();
+          
+           draino.empty().
+               from(
+                   new simple('data', 'end', ['i','am','first'], 5),
+                   new simple('data', 'end', ['second','stream','here'], 5),
+                   new simple('data', 'end', ['three','for','all'], 5)
+               ).to(target).
+               onEach('got1').
+               flush();
+          
+            target.on('got1',function (data) {
+                accumulate.push(data);
+                if (accumulate.length === 3) {
+                    this.emit('success', accumulate);
+                }
+            });
+               
+            return target;
+        },
+        'will chunk the streams to you.' : function (error, data) {
+            assert.strictEqual(data.length, 3);
+            assert.strictEqual(data[0], 'iamfirst');
+            assert.strictEqual(data[1], 'secondstreamhere');
+            assert.strictEqual(data[2], 'threeforall');
+        }
+    },
+    'After flushing all read streams with onDone()' : {
+        topic : function () {
+            var accumulate = [],
+                target = new EventEmitter();
+          
+           draino.empty().
+               from(
+                   new simple('data', 'end', ['i','am','first'], 5),
+                   new simple('data', 'end', ['second','stream','here'], 5),
+                   new simple('data', 'end', ['three','for','all'], 5)
+               ).to(target).
+               onEach('got1').
+               onDone('HotDAM').
+               flush();
+          
+            target.on('HotDAM',function (data) {
+                this.emit('success', 'worked');
+            });
+               
+            return target;
+        },
+        'the onDone event will fire.' : function (error, data) {
+            assert.strictEqual(data, 'worked');
         }
     },
     'Flushing to a callback' : {
         topic : function(){
             var s = new simple('data', 'end', ['one','two','three'], 5);
             
-            draino.flush().from(s).to(this.callback);
+            draino.empty(s).to(this.callback).flush();
         },
         'will return all the data.' : function(error, data) {
             assert.strictEqual(data, 'onetwothree');
@@ -174,86 +244,22 @@ vows.describe('how to use draino').addBatch(
     },
     'Flushing from a callback' : {
         topic : function(){
-            draino.flush().from(function(){
+            draino.empty().from(function(){
                 return 'onetwothree';
-            }).to(this.callback);
+            }).to(this.callback).flush();
         },
         'will return all the data.' : function(error, data) {
             assert.strictEqual(data, 'onetwothree');
         }
     },
-    'Flushing N read streams' : {
-        topic : function () {
-            var accumulate = [];
-            return draino.flush().from(
-                new simple('data', 'end', ['i','am','first'], 5),
-                new simple('data', 'end', ['second','stream','here'], 5),
-                new simple('data', 'end', ['three','for','all'], 5)
-            ).toMe('got1').on('got1',function (data) {
-                accumulate[accumulate.length] = data;
-                if (accumulate.length === 3) {
-                    this.emit('success', accumulate);
-                }
-            });
-        },
-        'will chunk the streams to you.' : function (error, data) {
-            assert.strictEqual(data.length, 3);
-            assert.strictEqual(data[0], 'iamfirst');
-            assert.strictEqual(data[1], 'secondstreamhere');
-            assert.strictEqual(data[2], 'threeforall');
-        }
-    },
-    'Flushing N read streams where the streams are in ONE array' : {
-        topic : function () {
-            var accumulate = [];
-            return draino.flush().from([
-                new simple('data', 'end', ['i','am','first'], 5),
-                new simple('data', 'end', ['second','stream','here'], 5),
-                new simple('data', 'end', ['three','for','all'], 5)
-            ]).toMe('got1').on('got1',function (data) {
-                accumulate[accumulate.length] = data;
-                if (accumulate.length === 3) {
-                    this.emit('success', accumulate);
-                }
-            });
-        },
-        'will chunk the streams to you.' : function (error, data) {
-            assert.strictEqual(data.length, 3);
-            assert.strictEqual(data[0], 'iamfirst');
-            assert.strictEqual(data[1], 'secondstreamhere');
-            assert.strictEqual(data[2], 'threeforall');
-        }
-    },
-    'Flushing N read streams to a callback' : {
-        topic : function () {
-            var accumulate = [],
-                self = this;
-             draino.flush().from(
-                new simple('data', 'end', ['i','am','first'], 5),
-                new simple('data', 'end', ['second','stream','here'], 5),
-                new simple('data', 'end', ['three','for','all'], 5)
-            ).to(function (error, data) {
-                accumulate[accumulate.length] = data;
-                if (accumulate.length === 3) {
-                    self.callback(null, accumulate);
-                }
-            });
-        },
-        'will chunk the streams to you.' : function (error, data) {
-            assert.strictEqual(data.length, 3);
-            assert.strictEqual(data[0], 'iamfirst');
-            assert.strictEqual(data[1], 'secondstreamhere');
-            assert.strictEqual(data[2], 'threeforall');
-        }
-    },
     'Flushing read streams and functions' : {
         topic : function () {
             var accumulate = [];
-            return draino.flush().from(
+            return draino.empty().from(
                 new simple('data', 'end', ['i','am','first'], 5),
                 function () { return 'secondstreamhere'; },
                 new simple('data', 'end', ['three','for','all'], 5)
-            ).toMe('got1').on('got1',function (data) {
+            ).toMe('got1').flush().on('got1',function (data) {
                 accumulate[accumulate.length] = data;
                 if (accumulate.length === 3) {
                     this.emit('success', accumulate);
@@ -280,7 +286,7 @@ vows.describe('how to use draino').addBatch(
                     5
                 );
             
-            return draino.flush(s).toMe();
+            return draino.empty(s).toMe().flush();
         },
         'will return all the data.' : function(error, data) {
             assert.ok(Array.isArray(data));
@@ -309,7 +315,7 @@ vows.describe('how to use draino').addBatch(
                port: port,
            }, function (response) {
                response.setEncoding('utf8');
-               draino.flush(response).to(self.callback)
+               draino.empty(response).to(self.callback).flush();
            });
         },
         'will return all the data.' : function(error, data) {
