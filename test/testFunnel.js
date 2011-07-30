ec077bfdecce340f9e1e31826d474bdc924809b2
seebees <seebees@gmail.com>
2011-07-29 19:38:01 -0700

    Completed tests
    Signed-off-by: seebees <seebees@gmail.com>

--- test/testFunnel.js
+++ test/testFunnel.js
@@ -7,6 +7,7 @@ var assert          = require('assert'),
         var writeCount = 0;
         this.writable = true;
         this.tmp = '';
+        this.startTime = new Date();
         
         this.write = function(chunk) {
             //console.log(chunk);
@@ -19,6 +20,7 @@ var assert          = require('assert'),
             }
         };
         this.end = function () {
+            this.stopTime = new Date();
             this.emit('success', this.tmp);
         };
     },
@@ -66,26 +68,25 @@ util.inherits(SimpleWrite, Stream);
 util.inherits(SimpleRead, Stream);
 util.inherits(ShinnyEmitter, process.EventEmitter);
 
-vows.describe('funnel').addBatch(
+vows.describe('draino.funnel').addBatch(
 {
     'the basic test objects' : {
         topic : function () {
-            var read  = new SimpleRead(['one', 'two', 'three'], 10);
+            var read  = new SimpleRead([' A1 ', ' A2 ', ' A3 '], 10);
             var write = new SimpleWrite();
             read.pipe(write);
             return write;
-        
         },
         'will pipe data' : function (data) {
             assert.ok(typeof data === 'string');
-            assert.strictEqual(data, 'onetwothree');
+            assert.strictEqual(data, ' A1  A2  A3 ');
         }
     },
-    'draino.funnel(ReadStream, WriteStream)' : {
+    'draino.funnel(ReadStream, WriteStream, {serial:true, buffer:false})' : {
         topic : function () {
             this.w = new SimpleWrite();
             this.newWrite = draino.funnel(
-                new SimpleRead(['A1', 'A2', 'A3'], 10),
+                new SimpleRead([' A1 ', ' A2 ', ' A3 '], 10),
                 this.w,
                 {serial:true, buffer:false}
             );
@@ -97,10 +98,10 @@ vows.describe('funnel').addBatch(
         },
         'will write the data to the WriteStream.' : function(data) {
             assert.ok(typeof data === 'string');
-            assert.strictEqual(data, 'A1A2A3');
+            assert.strictEqual(data, ' A1  A2  A3 ');
         }
     },
-    'qwer multipule draino.funnel(ReadStream, WriteStream, {buffer:false})' :{
+    'multipule draino.funnel(ReadStream, WriteStream, {serial:true, buffer:false})' :{
         topic : function () {
             this.newWrite           = new SimpleWrite();
             this.newWrite.funnel    = draino.SerialPump.prototype.funnel;
@@ -115,17 +116,17 @@ vows.describe('funnel').addBatch(
             draino.funnel(
                 new SimpleRead([' A1 ' , ' A2 ', ' A3 '], 10),
                 this.newWrite,
-                {buffer:false}
+                {serial:true, buffer:false}
             );
             draino.funnel(
                 new SimpleRead([' B1 ' ,' B2 ', ' B3 '], 5),
                 this.newWrite,
-                {buffer:false}
+                {serial:true, buffer:false}
             );
             draino.funnel(
                 new SimpleRead([' C1 ' , ' C2 ', ' C3 '], 20),
                 this.newWrite,
-                {buffer:false}
+                {serial:true, buffer:false}
             );
             
             return this.newWrite;
@@ -135,14 +136,18 @@ vows.describe('funnel').addBatch(
             
             assert.strictEqual(data, ' A1  A2  A3  B1  B2  B3  C1  C2  C3 ');
         },
-        'will emit pipe for each stream' : function () {
+        'will emit pipe for each stream in order' : function () {
             assert.ok(this.newWrite.myPipes.length === 3);
             assert.strictEqual(this.newWrite.currtmp[0], '');
             assert.strictEqual(this.newWrite.currtmp[1], ' A1  A2  A3 ');
             assert.strictEqual(this.newWrite.currtmp[2], ' A1  A2  A3  B1  B2  B3 ');
+        },
+        'will take serial time to complete' : function () {
+            var timeToComplete = this.newWrite.stopTime.getTime() - this.newWrite.startTime.getTime();
+            assert.ok(timeToComplete > 10*3 + 5*3 + 20*3);
         }
     },
-    'multipule draino.funnel(ReadStream, WriteStream, {serial:true})' :{
+    'multipule draino.funnel(ReadStream, WriteStream, {serial:true, buffer:true})' :{
         topic : function () {
             this.newWrite           = new SimpleWrite();
             this.newWrite.funnel    = draino.SerialPump.prototype.funnel;
@@ -157,17 +162,17 @@ vows.describe('funnel').addBatch(
             draino.funnel(
                 new SimpleRead([' A1 ' , ' A2 ', ' A3 '], 10),
                 this.newWrite,
-                {serial:true}
+                {serial:true, buffer:true}
             );
             draino.funnel(
                 new SimpleRead([' B1 ' ,' B2 ', ' B3 '], 20),
                 this.newWrite,
-                {serial:true}
+                {serial:true, buffer:true}
             );
             draino.funnel(
                 new SimpleRead([' C1 ' , ' C2 ', ' C3 '], 5),
                 this.newWrite,
-                {serial:true}
+                {serial:true, buffer:true}
             );
             
             return this.newWrite;
@@ -176,14 +181,19 @@ vows.describe('funnel').addBatch(
             assert.ok(typeof data === 'string');
             assert.strictEqual(data, ' A1  A2  A3  B1  B2  B3  C1  C2  C3 ');
         },
-        'will emit pipe for each stream' : function () {
+        'will emit pipe for each stream in order' : function () {
             assert.ok(this.newWrite.myPipes.length === 3);
             assert.strictEqual(this.newWrite.currtmp[0], '');
             assert.strictEqual(this.newWrite.currtmp[1], ' A1  A2  A3 ');
             assert.strictEqual(this.newWrite.currtmp[2], ' A1  A2  A3  B1  B2  B3 ');
+        },
+        'will take less then serial time to complete' : function () {
+            var timeToComplete = this.newWrite.stopTime.getTime() - this.newWrite.startTime.getTime();
+            //console.log(timeToComplete);
+            assert.ok(timeToComplete < 10*3 + 5*3 + 20*3);
         }
     },
-    'asdf multipule draino.funnel(ReadStream, WriteStream, {serial:false, buffer:true})' :{
+    'multipule draino.funnel(ReadStream, WriteStream, {serial:false, buffer:true})' :{
         topic : function () {
             this.newWrite           = new SimpleWrite();
             this.newWrite.funnel    = draino.SerialPump.prototype.funnel;
@@ -215,15 +225,103 @@ vows.describe('funnel').addBatch(
             
             return this.newWrite;
         },
-        'will write all the data to the WriteStream in order' : function (data) {
+        'will write all the data to the WriteStream in the order completed' : function (data) {
             assert.ok(typeof data === 'string');
             assert.strictEqual(data, ' A1  A2  A3  C1  C2  C3  B1  B2  B3 ');
         },
-        'will emit pipe for each stream' : function () {
+        'will emit pipe for each stream in the order processed.' : function () {
             assert.ok(this.newWrite.myPipes.length === 3);
             assert.strictEqual(this.newWrite.currtmp[0], '');
             assert.strictEqual(this.newWrite.currtmp[1], ' A1  A2  A3 ');
             assert.strictEqual(this.newWrite.currtmp[2], ' A1  A2  A3  C1  C2  C3 ');
+        },
+        'will take less then serial time to complete' : function () {
+            var timeToComplete = this.newWrite.stopTime.getTime() - this.newWrite.startTime.getTime();
+            //console.log(timeToComplete);
+            assert.ok(timeToComplete < 10*3 + 5*3 + 20*3);
+        }
+    },
+    'draino.funnel' : {
+        topic: function () {
+            var fs = require('fs');
+            
+            var times       = 21;
+            var bufferSize  = 1024*20;
+            var i;
+            
+            var pipeWrite   = new SimpleWrite();
+            var pipeStreams = [];
+            
+            var funnelWrite = new SimpleWrite();
+            
+            for (i = 0; i < times; i++) {
+                var tmp = fs.createReadStream(__filename, {bufferSize:bufferSize});
+                pipeStreams.push(tmp);
+                tmp.pause();
+            }
+            
+            var next = pipeStreams.shift();
+            
+            function nextPipe () {
+                var next = pipeStreams.shift();
+                if (next) {
+                    next.resume();
+                    next.pipe(pipeWrite, {end:false});
+                    next.on('end', nextPipe);
+                } else {
+                    pipeWrite.end();
+                }
+            }
+            
+            next.resume();
+            next.pipe(pipeWrite, {end:false});
+            next.on('end', nextPipe);
+            
+            var funnelWrite = new SimpleWrite();
+            
+            for (i = 0; i < times; i++) {
+                draino.funnel(
+                    fs.createReadStream(__filename, {bufferSize:bufferSize}),
+                    funnelWrite
+                );
+            }
+            
+            var monitor = new process.EventEmitter();
+            
+            var self = this
+            pipeWrite.on('success', function () {
+                self.pipeDone = true;
+                if (self.funnelDone) {
+                    self.callback(null, {
+                        pipe   : pipeWrite,
+                        funnel : funnelWrite
+                    });
+                }
+            });
+            
+            funnelWrite.on('success', function () {
+                self.funnelDone = true;
+                if (self.pipeDone) {
+                    self.callback(null, {
+                        pipe   : pipeWrite,
+                        funnel : funnelWrite
+                    });
+                }
+            });
+        },
+        'will be performant' : function (error, data) {
+            
+            //console.dir({
+            //    pipeStart   : data.pipe.startTime.getTime(),
+            //    pipeStop    : data.pipe.stopTime.getTime(),
+            //    funnelStart : data.funnel.startTime.getTime(),
+            //    funnelStop  : data.funnel.stopTime.getTime()
+            //})
+            
+            assert.ok(data.funnel.startTime = data.pipe.startTime);
+            assert.ok(data.funnel.stopTime  <= data.pipe.stopTime);
+            
+            //console.log('Faster:' + (data.pipe.stopTime.getTime() - data.funnel.stopTime.getTime()));
         }
     }
 }).export(module);
